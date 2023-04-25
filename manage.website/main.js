f8b5e7f4bdf55ba05cbaaca277a15129774501fe
sleepielle <pggisselle@gmail.com>
2023-04-25 05:35:54 -0600

    slider

--- manage.website/main.js
+++ manage.website/main.js
@@ -9,3 +9,10 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+});
