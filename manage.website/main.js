8730719374ccaa888952eac61eafbe9574863fc9
sleepielle <pggisselle@gmail.com>
2023-04-26 02:06:11 -0600

    fix nav and blobs

--- manage.website/main.js
+++ manage.website/main.js
@@ -1,13 +1,23 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const toggleIcon = document.querySelector(".toggle-icon");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
-  primaryHeader.toggleAttribute("data-overlay");
+  primaryHeader.toggleAtstribute("data-overlay");
+
+  if (
+    toggleIcon.getAttribute("src") ===
+    "/manage.website/images/icon-hamburger.svg"
+  ) {
+    toggleIcon.setAttribute("src", "/manage.website/images/icon-close.svg");
+  } else {
+    toggleIcon.setAttribute("src", "/manage.website/images/icon-hamburger.svg");
+  }
 });
 
 const slider = new A11YSlider(document.querySelector(".slider"), {
